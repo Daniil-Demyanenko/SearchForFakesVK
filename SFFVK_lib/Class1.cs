8538a16a6a5654826cd45aeb72c6cb3729b3d34f
Daniil-Demyanenko <daniil-demyanenko1@mail.ru>
2021-05-23 17:29:16 +0300

    Добавьте файлы проекта.

---
+++ SFFVK_lib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SFFVK_lib
+{
+    public class FakeSearcher
+    {
+        private string TOKEN;
+
+        public FakeSearcher(string VK_Token)
+        {
+            TOKEN = VK_Token;
+        }
+
+    }
+}
---
+++ SearchForFakesVK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SearchForFakesVK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
