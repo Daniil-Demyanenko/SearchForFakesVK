1c4e5c6019f6dfb977f640d1cd6077f644741571
Daniil-Demyanenko <daniil-demyanenko1@mail.ru>
2021-05-24 03:36:04 +0300

    Добавлен пример работы с библиотекой

--- SearchForFakesVK/Program.cs
+++ SearchForFakesVK/Program.cs
@@ -12,6 +12,7 @@ namespace SearchForFakesVK
             Color Err = new Color(ConsoleColor.Red);
             Color Dialog = new Color(ConsoleColor.Cyan);
             Color Log = new Color(ConsoleColor.Blue);
+            int User_id;
 
             FakeSearcher Searcher;
             string Token = GetVKToken();
@@ -27,6 +28,31 @@ namespace SearchForFakesVK
                 return;
             }
 
+            Dialog.Print("Введите id пользователя ВКонтакте, которого нужно исследовать на фейковые страницы.\n" +
+                "(только цифровой id)\n>>");
+
+            try
+            {
+                User_id = int.Parse(Console.ReadLine());
+            }
+            catch {
+                Err.PrintLine("Введён неверный id");
+                return;
+            }
+
+            Dialog.PrintLine("Страницы могут быть не найдены, если настройки приватности ииследуемой страницы запрещают просмотр групп.");
+            Log.PrintLine("Начато сканирование. Это может занять несколько минут.");
+
+            var users = Searcher.InformativePredict(User_id);
+
+            Dialog.PrintLine($"Всего у пользователя {Searcher.UserGroupCount} групп.");
+            Dialog.PrintLine("Кол-во совпавшах групп и id страниц (топ-20):");
+
+            for (int i = 0; i < 20; i++)
+            {
+                Dialog.PrintLine($"{users[i].count}\t{users[i].user_id}");
+            }
+            Log.PrintLine("\nНажмите любую клавишу для завершения...");
             Console.ReadKey();
         }
 
